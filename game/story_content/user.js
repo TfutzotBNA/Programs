c014149e97fdaca038417382242aefda72b8513d
TfutzotBNA <116673814+TfutzotBNA@users.noreply.github.com>
2022-12-07 21:36:08 +0200

    Add files via upload

--- game/story_content/user.js
+++ game/story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6B0hik0RGKt":
+      case "5e9dxxbQZFH":
         Script1();
         break;
   }
@@ -10,45 +10,19 @@ function ExecuteScript(strId)
 
 function Script1()
 {
-  var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-head.appendChild(script)
-var player = GetPlayer();
- 
-//PLACE YOUR WEB APP URL
-WEB_APP_URL = "https://script.google.com/a/macros/bna.org.il/s/AKfycbzDz8QePBYeisLr1IbfqhgVfek-SxuGinWuAfZ9hpQimrQ0YD-1pf0zYUZjDGeUUJWscQ/exec";
- 
-// STORE ARTICULATE STORYLINE VARIABLES
-// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
-// ATTENTION: Use a comma if you use multiple Storyline variables
-storyline =
-{
- "TimeStamp" : new Date().toJSON().slice(0,10), //STORE DATE
- "שם" : player.GetVar("שם"),
- "טלפון" : player.GetVar("טלפון"),
- "סניפים" : player.GetVar("סניפים"),
- "מחוז" : player.GetVar("מחוז")
- }
-//DELAY SO JQUERY LIBRARY IS LOADED
-setTimeout(function (){
- 
-//Export to Google
-$.ajax({
-url:"https://script.google.com/a/macros/bna.org.il/s/AKfycbzDz8QePBYeisLr1IbfqhgVfek-SxuGinWuAfZ9hpQimrQ0YD-1pf0zYUZjDGeUUJWscQ/exec",
-type: "POST",
-data : storyline,
-success: function(data)
-{
-console.log(data);
-},
-error: function(err) {
-console.log('Error:', err);
-}
-});
-return false;
-}, 1000);
+  // Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbz1jcLdPAKCJHVAia7BwL9-UgnAxfVEcAY8jnS8ydRig6SXMG-naM3CA6Ryu-ix6QpJuw/exec";
 
+const player = GetPlayer();
+let feedBack = player.GetVar("name"); // Replace "UserFeedback" with your Storyline Variable.
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors', 
+  cache: 'no-cache', 
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: feedBack}) 
+});
 }
 
