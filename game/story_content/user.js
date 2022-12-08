a5d6e5233e25861aa52333ca6c4130d29deabed3
TfutzotBNA <116673814+TfutzotBNA@users.noreply.github.com>
2022-12-08 18:17:20 +0200

    Add files via upload

--- game/story_content/user.js
+++ game/story_content/user.js
@@ -2,8 +2,11 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5e9dxxbQZFH":
+      case "67WgCkJF5Ha":
         Script1();
+        break;
+      case "5g5lwlXbRvF":
+        Script2();
         break;
   }
 }
@@ -15,6 +18,26 @@ const url = "https://script.google.com/macros/s/AKfycbz1jcLdPAKCJHVAia7BwL9-UgnA
 
 const player = GetPlayer();
 let feedBack = player.GetVar("name"); // Replace "UserFeedback" with your Storyline Variable.
+
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors', 
+  cache: 'no-cache', 
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: feedBack}) 
+});
+}
+
+function Script2()
+{
+  // Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbz1jcLdPAKCJHVAia7BwL9-UgnAxfVEcAY8jnS8ydRig6SXMG-naM3CA6Ryu-ix6QpJuw/exec";
+
+const player = GetPlayer();
+let feedBack = player.GetVar("snif"); // Replace "UserFeedback" with your Storyline Variable.
+
 
 fetch(url,{
   method: 'POST',
