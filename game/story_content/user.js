ccaec56667263c1b332483a0bf880778a4fd5299
TfutzotBNA <116673814+TfutzotBNA@users.noreply.github.com>
2022-12-13 18:04:33 +0200

    Add files via upload

--- game/story_content/user.js
+++ game/story_content/user.js
@@ -2,101 +2,61 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5hIdNVfQ3Kz":
+      case "6fdzyuRwcCu":
         Script1();
         break;
-      case "6ELKBmDV9vi":
+      case "6Cbqt6UZU6r":
         Script2();
         break;
-      case "6BR9nYJzgkH":
+      case "6Qpv0XXWgxK":
         Script3();
         break;
-      case "6VItzVsVYTj":
+      case "6CIN3h1iW8r":
         Script4();
         break;
-      case "6F10ZJk1fsR":
+      case "5jkBYLsQzGf":
         Script5();
         break;
-      case "6MPV2ipPiKx":
+      case "6UaXzgwYHBI":
         Script6();
-        break;
-      case "5iNkUOgdxXO":
-        Script7();
-        break;
-      case "6K3gshq2P41":
-        Script8();
-        break;
-      case "5Y6suAjcpHC":
-        Script9();
         break;
   }
 }
 
 function Script1()
 {
-  if(void 0===window.stencilsheets){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-sheets-init-v2?authtool=sl&v=1670346495&chart=NzE4fDM1NDl8ZDU4NWVjYTY5MzA4NGRmYmY5ZmYzMjg2NjU4YzYyYzQ";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],n=document.createElement("script");t.appendChild(n),n.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+  if(void 0===window.stencilsheets){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-sheets-init-v2?authtool=sl&v=1670694374&chart=NzMyfDM1NjF8OGFhODgzZThjY2EwZmZmNDQ5ODRlYTI4YWRlNGVmMmE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],n=document.createElement("script");t.appendChild(n),n.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+        
         
 }
 
 function Script2()
 {
   var player = GetPlayer();
-SheetsSet('שם', player.GetVar('name'));
-
+SheetsSet('אוסטריה', player.GetVar('תשובה_אוסטריה'));
 }
 
 function Script3()
 {
   var player = GetPlayer();
-SheetsSet('טלפון', player.GetVar('Tel'));
-
+SheetsSet('אטלנטה', player.GetVar('תשובה_אטלנטה'));
 }
 
 function Script4()
 {
   var player = GetPlayer();
-SheetsSet('סניף', player.GetVar('snif'));
-
+SheetsSet('בלגיה', player.GetVar('בלגיה_תשובה'));
 }
 
 function Script5()
 {
   var player = GetPlayer();
-SheetsSet('עיר', player.GetVar('city'));
-
+SheetsSet('בלגיה', player.GetVar('בלגיה_תשובה'));
 }
 
 function Script6()
 {
   var player = GetPlayer();
-SheetsSet('מחוז', player.GetVar('district'));
-
-}
-
-function Script7()
-{
-  var today = new Date();
-var date=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-var dateTime = date+' '+time;
-
-SheetsSet('תאריך הרשמה', dateTime);
-
-}
-
-function Script8()
-{
-  var player = GetPlayer();
-SheetsSet('שם', player.GetVar('name'));
-
-}
-
-function Script9()
-{
-  var player = GetPlayer();
-SheetsSet('סניף', player.GetVar('snif'));
-
+SheetsSet('בלגיה', player.GetVar('בלגיה_תשובה'));
 }
 
