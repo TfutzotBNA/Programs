507c4012dc73f9dd531e91f4bd63f22f74b3837b
TfutzotBNA <116673814+TfutzotBNA@users.noreply.github.com>
2023-02-24 14:59:41 +0200

    Add files via upload

---
+++ AroundTheWorld_BA-DiasporaWeek/story_content/user.js
@@ -0,0 +1,160 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6hQj70fJqEp":
+        Script1();
+        break;
+      case "6jiS9Ng8zw5":
+        Script2();
+        break;
+      case "6aXseeXVF9s":
+        Script3();
+        break;
+      case "6h2QnhPfEol":
+        Script4();
+        break;
+      case "6j5vzOSdfLU":
+        Script5();
+        break;
+      case "6FZpV1GQ9tn":
+        Script6();
+        break;
+      case "6Wm25yzy7Gf":
+        Script7();
+        break;
+      case "5q1GUWMDeD5":
+        Script8();
+        break;
+      case "6nDxomCDr1r":
+        Script9();
+        break;
+      case "5l3pFxkFvTB":
+        Script10();
+        break;
+      case "5gGxB2JHkh3":
+        Script11();
+        break;
+      case "6UVNgW22lyK":
+        Script12();
+        break;
+      case "6E2llZMtapn":
+        Script13();
+        break;
+      case "5aUqs0EvWJ9":
+        Script14();
+        break;
+      case "6N0bHm7evyc":
+        Script15();
+        break;
+      case "5eqUUAGBwbA":
+        Script16();
+        break;
+      case "5zAzCjfQBxH":
+        Script17();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencilsheets){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-sheets-init-v2?authtool=sl&v=1671013284&chart=NzUzfDM1NjJ8NjliZDhlNzExYWRhM2E1OTMyNDc0NGVmOWQ2NTQ5ZjE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var e=this.responseText,t=document.getElementsByTagName("head")[0],n=document.createElement("script");t.appendChild(n),n.appendChild(document.createTextNode(e)),window.stencilsheets=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+        
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+SheetsSet('אוסטריה', player.GetVar('תשובה_אוסטריה'));
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+SheetsSet('אטלנטה', player.GetVar('תשובה_אטלנטה'));
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+SheetsSet('בלגיה', player.GetVar('תשובה_בלגיה'));
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+SheetsSet('בריטניה', player.GetVar('תשובה_בריטניה'));
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+SheetsSet('גרמניה', player.GetVar('תשובה_גרמניה'));
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+SheetsSet('דטרויט', player.GetVar('תשובה_דטרויט'));
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+SheetsSet('דרום אפריקה', player.GetVar('תשובה_דרוםאפריקה'));
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+SheetsSet('הולנד', player.GetVar('תשובה_הולנד'));
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+SheetsSet('יוסטון', player.GetVar('תשובה_יוסטון'));
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+SheetsSet('מונטריאול', player.GetVar('תשובה_מונטריאול'));
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+SheetsSet('מילאנו', player.GetVar('תשובה_מילאנו'));
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+SheetsSet('מקסיקו', player.GetVar('תשובה_מקסיקו'));
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+SheetsSet('צרפת', player.GetVar('תשובה_צרפת'));
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+SheetsSet('קליבלנד', player.GetVar('תשובה_קליבלנד'));
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+SheetsSet('רומא', player.GetVar('תשובה_רומא'));
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+SheetsSet('ריו', player.GetVar('תשובה_ריו'));
+}
+
